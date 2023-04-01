375be78cdbe897b7660c3a1342465b927bef2d85
vnanhkhoa <anhkhoadn20@gmail.com>
2023-04-01 10:16:48 +0700

    no message

--- LearnMockk/app/src/main/java/com/khoavna/learnmockk/Preference.java
+++ LearnMockk/app/src/main/java/com/khoavna/learnmockk/Preference.java
@@ -3,10 +3,8 @@ package com.khoavna.learnmockk;
 import android.content.Context;
 
 public class Preference {
-    private final Context context;
     public boolean logic;
 
     public Preference(Context context) {
-        this.context = context;
     }
 }
